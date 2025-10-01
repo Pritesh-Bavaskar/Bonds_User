75501b0b938e5495945eed4f203e3c31205e9ab6
abhi <kolheroshan508@gmail.com>
2025-10-01 10:12:55 +0530

    News insight Card css fix

--- src/sections/news-insights/news-insight-card.js
+++ src/sections/news-insights/news-insight-card.js
@@ -6,13 +6,41 @@ export default function NewsInsightCard({ title, date, subtitle, subtitleBold })
   return (
     <Card sx={{ width: '100%', height: 'auto', borderRadius: 1 }}>
       <CardContent>
-        <Typography variant="h6" gutterBottom>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{
+            fontFamily: 'Public Sans',
+            fontWeight: 500,
+            fontStyle: 'normal',
+            fontSize: '16px',
+            lineHeight: '33.06px',
+            letterSpacing: '0px',
+          }}
+        >
           {title}
         </Typography>
         <Typography variant="body2" color="#003289" gutterBottom>
           {date}
         </Typography>
-        <Typography variant="body1" gutterBottom>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{
+            fontFamily: 'Public Sans',
+            fontWeight: 300,
+            fontStyle: 'normal',
+            fontSize: '12px',
+            lineHeight: '18.51px',
+            letterSpacing: '0px',
+            maxHeight: 'calc(18.51px * 2)',
+            overflowY: 'auto',
+            scrollbarWidth: 'none',
+            '&::-webkit-scrollbar': {
+              display: 'none',
+            },
+          }}
+        >
           {subtitle}
         </Typography>
         <Typography
